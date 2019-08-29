412b0c82bd7ae128ebb3839e6b5525d9578e34fb
W4tchtower <39549124+W4tchtower@users.noreply.github.com>
2019-08-28 22:58:27 -0500

    Add files via upload

---
+++ camera.lua
@@ -0,0 +1,25 @@
+camera = {}
+camera.x = 0
+camera.y = 0
+
+
+-- The two required functions for the camera to update at all
+function camera:set()
+    lg.push()
+    lg.translate(-self.x, -self.y)
+end
+function camera:unset()
+    lg.pop()
+end
+
+
+-- Camera movement
+function camera:set_position(_x, _y)
+    self.x = _x or self.x
+    self.y = _y or self.y
+end
+function camera:move(_x, _y)
+    self.x = self.x + (_x or 0)
+    self.y = self.y + (_y or 0)
+end
+
